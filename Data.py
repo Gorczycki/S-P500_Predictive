7829c06bbc0eab1eb5daab977d07b1f0d7e447c7
Hunter28321 <161870145+Hunter28321@users.noreply.github.com>
2024-03-01 13:03:38 -0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -5,7 +5,7 @@ import seaborn
 
 #imports the data 
 import csv
-file = open('spy.csv')
+file = open('SPYdata.csv')
 type = file
 csvreader = csv.reader(file)
 header = []
@@ -15,35 +15,61 @@ header
 rows = []
 for row in csvreader:
     rows.append(row)
-rows.pop()
+
 
 file.close()
+import csv
+file = open('VIXdata.csv')
+type = file
+csvreader = csv.reader(file)
+vheader = []
+vheader = next(csvreader)
+vheader
+
+vrows = []
+for row in csvreader:
+    vrows.append(row)
+
 
 
 numbers = []
-
+#numbers = np.array(numbers)
 
 #Takes out the unessecary string values to convert them later
+#for i in range(len(rows)):
+    #rows[i][0] = rows[i][0].strip().strip(r"'\"")
+    #rows[i][1] = rows[i][1].strip('\\\"')
+    #rows[i][2] = rows[i][2].strip('\\\"')
+    #if i != len(rows) - 1:
+        #rows[i].pop()
+#rows = np.array(rows)
+#vrows = np.array(vrows)
+
 for i in range(len(rows)):
-    rows[i][0] = rows[i][0].strip().strip(r"'\"")
-    rows[i][1] = rows[i][1].strip('\\\"')
-    rows[i][2] = rows[i][2].strip('\\\"')
-    if i != len(rows) - 1:
-        rows[i].pop()
+    for j in range(len(vrows)):
+        if rows[i][0] == vrows[j][0]:
+            rows[i].append(vrows[j][1])
+            
+
+
+    
+    
+
     
 #Puts the data into a new array in order to manipulate
 for item in rows:
     numbers.append(item)
 
-#takes out the date leaving with closing price and volume
+
+#takes out the date leaving with closing price, volume, and vix
 #then converts this array to a floating point array
 for i in range(len(rows)):
     numbers[i].pop(0)
     j = 0
-    while j < 2:
+    while j < 3:
         numbers[i][j] = float(numbers[i][j])
         j+=1
-    print(numbers[i][1])
+    
 #changes to a numpy array then plots it
 numbers = np.array(numbers)
 #pyplot.scatter(numbers[:,0],numbers[:,1])
@@ -53,7 +79,9 @@ numbers = np.array(numbers)
 #centers the data at 0
 mean = np.mean(numbers, axis= 0)
 mean_numbers = numbers - mean
-print("Mean ", mean[1])
-pyplot.scatter(mean_numbers[:,0],mean_numbers[:,1])
-pyplot.grid(True)
-pyplot.show()
+print("Mean ", mean)
+#pyplot.scatter(mean_numbers[:,0],mean_numbers[:,1], mean_numbers[:,2])
+#pyplot.grid(True)
+#pyplot.show()
+
+
