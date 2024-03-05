069c2ecc59084ff5cb0d89eb76dc8c544d4bba4b
BlakeZurman <161869712+BlakeZurman@users.noreply.github.com>
2024-03-05 15:14:09 -0500

    Add files via upload

---
+++ LSTM_net.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+
+data = pd.read_csv('SPYdata.csv')
+closing_prices = data['Close'].values
+scaler = MinMaxScaler(feature_range=(0, 1))
+closing_prices_scaled = scaler.fit_transform(closing_prices.reshape(-1, 1))
+window_size = 10  
+
+X, y = [], []
+for i in range(len(closing_prices_scaled) - window_size):
+    X.append(closing_prices_scaled[i:i+window_size])
+    y.append(closing_prices_scaled[i+window_size])
+
+X, y = np.array(X), np.array(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+model.add(LSTM(units=50))
+model.add(Dense(units=1))
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+model.fit(X_train, y_train, epochs=100, batch_size=32)
+
+train_loss = model.evaluate(X_train, y_train, verbose=0)
+test_loss = model.evaluate(X_test, y_test, verbose=0)
+
+print("Train Loss:", train_loss)
+print("Test Loss:", test_loss)
+
+train_predictions = model.predict(X_train)
+test_predictions = model.predict(X_test)
+
+train_predictions = scaler.inverse_transform(train_predictions)
+test_predictions = scaler.inverse_transform(test_predictions)
+
+print("Train Predictions:", train_predictions[:5])
+print("Test Predictions:", test_predictions[:5])
